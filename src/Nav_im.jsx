abdf3b40d3189db11f62b9fc6ba46b2b8bc274b4
ARSBEGINS78 <abdulra1919.sheikh@gmail.com>
2026-07-26 20:47:45 +0530

    Add alt attribute to main image in Nav_im component
    Added alt attribute to the main image for accessibility.

--- src/Nav_im.jsx
+++ src/Nav_im.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nav_im = ({ scrollToSection, homeRef, aboutRef, contactusRef, pricetierRef, formRef, logo}) => {
   return (
     <div>
-          <img className='h-screen w-full object-cover' src="https://images.unsplash.com/photo-1553877522-43269d4ea984?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZGlnaXRhbCUyMG1hcmtldGluZ3xlbnwwfHwwfHx8MA%3D%3D"/>
+          <img className='h-screen w-full object-cover' src="https://images.unsplash.com/photo-1553877522-43269d4ea984?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZGlnaXRhbCUyMG1hcmtldGluZ3xlbnwwfHwwfHx8MA%3D%3D" alt="Digi"/>
               <img className='absolute top-2 left-2 md:left-4 h-24 md:h-40 z-30' src={logo}/>      
           <nav className='absolute top-0 w-full p-4 md:p-8 flex justify-end gap-4 md:gap-8 text-white font-bold text-sm md:text-lg z-10'>
           <button onClick={() => scrollToSection(homeRef)} className='hover:text-orange-500 transition-colors'>Home</button>
